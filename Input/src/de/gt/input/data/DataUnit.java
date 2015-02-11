5c5bec524455c027273bc5bc4057e7ea92500f3d
Kevin Neumeyer <k.neumeyer@gamma-team.de>
2015-02-11 13:23:50 +0100

    Klasse für Datenwerte erstellt.

--- Input/src/de/gt/input/data/DataUnit.java
+++ Input/src/de/gt/input/data/DataUnit.java
@@ -11,6 +11,10 @@ package de.gt.input.data;
  */
 public abstract class DataUnit {
 
+    private Double doubleValue;
+    private String stringValue;
+    private Long longValue;
+
     private DataType type;
 
     public abstract String getString();
@@ -18,4 +22,35 @@ public abstract class DataUnit {
     public abstract Long getLong();
 
     public abstract Double getDouble();
+
+    public DataUnit(Double doubleValue) {
+        this.doubleValue = doubleValue;
+        this.type = DataType.DOUBLE;
+    }
+
+    public DataUnit(String stringValue) {
+        this.stringValue = stringValue;
+        this.type = DataType.STRING;
+    }
+
+    public DataUnit(Long longValue) {
+        this.longValue = longValue;
+        this.type = DataType.LONG;
+    }
+
+    public Double getDoubleValue() {
+        return doubleValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public Long getLongValue() {
+        return longValue;
+    }
+
+    public DataType getType() {
+        return type;
+    }
 }
