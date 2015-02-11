08219af7d65e0e5fab16e805e92a410d0ebd399b
Robin Bley <robin-bley@hotmail.de>
2015-02-11 11:50:42 +0100

    Anfertigung des DataFormat-Interface

--- Input/src/de/gt/input/dataformat/DataFormat.java
+++ Input/src/de/gt/input/dataformat/DataFormat.java
@@ -6,11 +6,23 @@ package de.gt.input.dataformat;
  */
 public interface DataFormat {
     
-    
+    /**
+     * Prueft ob Daten vorhanden sind.
+     * 
+     * @return Liefert true, wenn Datenvorhanden sind. Andernfalls wird false zurueckgegeben.
+     */
     public boolean hasData();
     
     
-   
+    /**
+     * Parst Daten vom vorgesehenen Datentyp
+     * 
+     * @return Liefert Eine Data-Objekt zurueck, welches die geparsten Daten enthaelt
+     */
+    public Data paseData();
+    
+    
+    
     
     
     
