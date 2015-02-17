f1dc860035073a6c2d0600e14bc53dfd041cb5bf
Kevin Neumeyer <k.neumeyer@gamma-team.de>
2015-02-17 15:48:12 +0100

    Unused Import entfernt und reformatiert

--- Input/src/de/gt/input/dataformat/DataFormat.java
+++ Input/src/de/gt/input/dataformat/DataFormat.java
@@ -1,32 +1,26 @@
 package de.gt.input.dataformat;
 
 import de.gt.input.data.DataUnit;
-import de.gt.input.sources.DataSource;
 
 /**
  *
  * @author Robin
  */
 public interface DataFormat {
-    
+
     /**
      * Prueft ob Daten vorhanden sind.
-     * 
-     * @return Liefert true, wenn Datenvorhanden sind. Andernfalls wird false zurueckgegeben.
+     *
+     * @return Liefert true, wenn Datenvorhanden sind. Andernfalls wird false
+     * zurueckgegeben.
      */
     public boolean hasData();
-    
-    
+
     /**
      * Parst Daten vom vorgesehenen Datentyp
-     * 
-     * @return Liefert Eine Data-Objekt zurueck, welches die geparsten Daten enthaelt
+     *
+     * @return Liefert Eine Data-Objekt zurueck, welches die geparsten Daten
+     * enthaelt
      */
     public DataUnit parseData();
-    
-    
-    
-    
-    
-    
 }
