aa3fb1e6fa230a6ea8eff4b74e003a38ff72df75
Robin Bley <robin-bley@hotmail.de>
2015-03-03 16:22:32 +0100

    TXTExporter

---
+++ Export/src/de/gt/export/TxtExporter.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gt.export;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+
+public class TxtExporter implements Exporter {
+
+    /**
+     * Liefert einen existierenden Pfad einer CSV-Datei zurueck.
+     *
+     * @param output Datei, aus der der Pfad gelsen wird
+     * @return Ein Pfad einer CSV-Datei
+     */
+    private String getPath(File output) {
+        String path;
+        if (output == null) {
+
+            StringBuilder buffer = new StringBuilder();
+            buffer.append(System.getProperty("user.dir"));
+            buffer.append(String.valueOf(new Timestamp(new java.util.Date().getTime())));
+            buffer.append(".csv");
+            path = buffer.toString();
+
+        } else if (!output.getParent().endsWith(".txt")) {
+            path = output.getPath() + ".txt";
+        } else {
+            path = output.getPath();
+        }
+        return path;
+    }
+
+    @Override
+    public boolean exportData(Map<String, List> data, File output) {
+        String path = getPath(output);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+            for (int i = 0; i < data.get(data.keySet().toArray()[0]).size(); i++) {
+                StringBuffer buffer = new StringBuffer("%-15s %15s %n");
+
+                for (String key : data.keySet()) {
+                    if (data.get(key).size() <= i) {
+                        buffer.append(", " + data.get(key).get(i));
+                    } else {
+                        buffer.append(", NULL");
+                    }
+                }
+                buffer.append(buffer.toString());
+                buffer.append(System.getProperty("line.separator"));
+            }
+
+            writer.close();
+
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
