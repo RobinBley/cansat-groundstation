ac3838099ddf3dfdb31cb8ac554a2cd2d9b246a2
Robin Bley <robin-bley@hotmail.de>
2015-03-10 16:15:13 +0100

    init DataLogger

---
+++ Input/src/de/gt/input/logging/Datalogger.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gt.input.logging;
+
+/**
+ *
+ * @author Robin
+ */
+public class Datalogger {
+    private Expoerter expoerter;
+    private String path;
+
+    public Datalogger(Expoerter expoerter, String path) {
+        this.expoerter = expoerter;
+        this.path = path;
+    }
+    
+    public boolean LogData(){
+        
+        
+        return false;
+        
+    }
+    
+    
+}
