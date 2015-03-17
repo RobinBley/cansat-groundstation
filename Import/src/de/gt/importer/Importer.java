66cb63c99fdd45380c5a550a794c3f9d214a53f0
mhuisi <scroot@freenet.de>
2015-03-17 16:04:52 +0100

    Fixed import namebase

--- Import/src/de/gt/importer/Importer.java
+++ Import/src/de/gt/importer/Importer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.gt.input;
+package de.gt.importer;
 
 import java.io.File;
 import java.util.List;
