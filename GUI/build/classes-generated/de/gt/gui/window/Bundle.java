d0650c37b82501df3057b2ef545b2777052db414
mhuisi <scroot@freenet.de>
2015-03-17 17:29:20 +0100

    Added TextDisplay component

--- GUI/build/classes-generated/de/gt/gui/window/Bundle.java
+++ GUI/build/classes-generated/de/gt/gui/window/Bundle.java
@@ -3,6 +3,20 @@ package de.gt.gui.window;
 @javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
 class Bundle {
     /**
+     * @return <i>TextDisplay</i>
+     * @see TextDisplayTopComponent
+     */
+    static String CTL_TextDisplayAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TextDisplayAction");
+    }
+    /**
+     * @return <i>TextDisplay Window</i>
+     * @see TextDisplayTopComponent
+     */
+    static String CTL_TextDisplayTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TextDisplayTopComponent");
+    }
+    /**
      * @return <i>Visualization</i>
      * @see VisualizationTopComponent
      */
@@ -15,6 +29,13 @@ class Bundle {
      */
     static String CTL_VisualizationTopComponent() {
         return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_VisualizationTopComponent");
+    }
+    /**
+     * @return <i>This is a TextDisplay window</i>
+     * @see TextDisplayTopComponent
+     */
+    static String HINT_TextDisplayTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_TextDisplayTopComponent");
     }
     /**
      * @return <i>This is a Visualization window</i>
