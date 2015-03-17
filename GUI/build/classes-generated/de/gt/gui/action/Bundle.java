aeb024cba1f2771199a541489ebdf2de3caeaf46
Kevin Neumeyer <kevin@kpro-media.de>
2015-03-17 12:26:46 +0100

    Warum auch immer

---
+++ GUI/build/classes-generated/de/gt/gui/action/Bundle.java
@@ -0,0 +1,27 @@
+package de.gt.gui.action;
+/** Localizable strings for {@link de.gt.gui.action}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Add</i>
+     * @see AddSatelliteAction
+     */
+    static String CTL_AddSatelliteAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_AddSatelliteAction");
+    }
+    /**
+     * @return <i>Manage</i>
+     * @see ManageSatellitesAction
+     */
+    static String CTL_ManageSatellitesAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_ManageSatellitesAction");
+    }
+    /**
+     * @return <i>Satellites</i>
+     * @see SatelliteManager
+     */
+    static String CTL_SatelliteManager() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_SatelliteManager");
+    }
+    private void Bundle() {}
+}
---
+++ GUI/build/classes-generated/de/gt/gui/window/Bundle.java
@@ -0,0 +1,27 @@
+package de.gt.gui.window;
+/** Localizable strings for {@link de.gt.gui.window}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Visualization</i>
+     * @see VisualizationTopComponent
+     */
+    static String CTL_VisualizationAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_VisualizationAction");
+    }
+    /**
+     * @return <i>Visualization Window</i>
+     * @see VisualizationTopComponent
+     */
+    static String CTL_VisualizationTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_VisualizationTopComponent");
+    }
+    /**
+     * @return <i>This is a Visualization window</i>
+     * @see VisualizationTopComponent
+     */
+    static String HINT_VisualizationTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_VisualizationTopComponent");
+    }
+    private void Bundle() {}
+}
