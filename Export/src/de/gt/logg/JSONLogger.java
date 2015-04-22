17bbd7d5849604b01703bb80d56b85d56b520504
Robin Bley <robin-bley@hotmail.de>
2015-04-22 15:56:13 +0200

    init JSONLogger

---
+++ Export/src/de/gt/logg/JSONLogger.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gt.logg;
+
+import de.gt.export.Exporter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
+
+public class JSONLogger implements Exporter {
+    
+    @Override
+    public boolean exportData(Map<String, List<Object>> data, File output) {
+        //Wenn das uebergebene File oder die uebergebene Map null ist, wird false zurueckgegeben.
+        if (output == null || data == null) {
+            return false;
+        }
+        try {
+            //Es wird ein FileWriter erzeugund, um das uebergebene File zu beschreiben.
+            FileWriter writer = new FileWriter(output, true);
+            
+            JSONObject jData = new JSONObject();
+            for (String key : data.keySet()) {
+                jData.put(key, data.get(key));
+            }
+            writer.write(jData.toString());
+            
+        } catch (Exception e) {
+            Logger.getLogger(JSONLogger.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+        
+        return true;
+    }
+    
+}
