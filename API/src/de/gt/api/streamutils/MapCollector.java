c5b1eb079b4cbc433d830475421b2c4337d3719d
mhuisi <marc.huisinga@freenet.de>
2015-04-29 20:13:24 +0200

    Fixed incomplete commit ...

--- API/src/de/gt/api/streamutils/MapCollector.java
+++ API/src/de/gt/api/streamutils/MapCollector.java
@@ -1,15 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package de.gt.api.streamutils;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 /**
- *
+ * Creates entry stream collectors
  * @author mhuisi
  */
-public class MapCollector {
+public class MapCollector<K, V> {
 
+    /**
+     * Creates a new collector
+     * which allows to collect 
+     * a stream of Entries<K, V> into
+     * a a Map<K, V>.
+     * @param <K>
+     * @param <V>
+     * @return 
+     */
+    public static <K, V> Collector<Entry<K, V>, ?, Map<K, V>> create() {
+	return Collectors.toMap(Entry::getKey, Entry::getValue);
+    }
+    
 }
