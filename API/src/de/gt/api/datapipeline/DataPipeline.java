33d9ea03019b78773308d05c652b5c023ebe7e52
Kevin Neumeyer <kevin@kpro-media.de>
2015-05-19 16:10:09 +0200

    Interface für Datapipeline (für Lookup Communication);

---
+++ API/src/de/gt/api/datapipeline/DataPipeline.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gt.api.datapipeline;
+
+import de.gt.api.input.dataformat.DataFormat;
+import de.gt.api.relay.Receiver;
+import de.gt.api.relay.Relay;
+import de.gt.api.sources.DataSource;
+
+/**
+ *
+ * @author Kevin
+ */
+public interface DataPipeline {
+
+    /**
+     * Tauscht den Parser aus und bindet den neuen Parser an die Pipeline an.
+     *
+     * @param newParser
+     */
+    public void exchangeParser(DataFormat newParser);
+
+    /**
+     * Tauscht das Relay aus und registriert die Komponenten beim neuen Relay.
+     *
+     * @param newRelay
+     */
+    public void exchangeRelay(Relay newRelay);
+
+    /**
+     * Tauscht die Datenquelle aus und linkt die neue Quelle an den Rest der
+     * Pipeline
+     *
+     *
+     * @param newSource
+     */
+    public void exchangeSource(DataSource newSource);
+
+    /**
+     * Registriert einen neuen Komponenten für den Datenempfang über die
+     * Pipeline
+     *
+     * @param receiver
+     */
+    public void registerDataReceiver(Receiver receiver);
+
+    /**
+     * Entfernt einen Receiver wieder vom Ende der Pipeline, sodass der Receiver
+     * keine weiteren Daten über die Pipeline empfängt.
+     *
+     * @param receiver
+     */
+    public void unregisterDataReceiver(Receiver receiver);
+}
