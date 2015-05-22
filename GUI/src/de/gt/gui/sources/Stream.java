84be33d807a3c163c5459cb16444a86f561cf701
Kevin Neumeyer <kevin@kpro-media.de>
2015-05-22 10:49:31 +0200

    Barharharharhar Stream und Serial gerefactored

--- GUI/src/de/gt/gui/sources/Stream.java
+++ GUI/src/de/gt/gui/sources/Stream.java
@@ -13,7 +13,7 @@ import java.util.Deque;
  *
  * @author mhuisi
  */
-public class Stream implements DataSource{
+public class Stream {
 
     private DataFormat formatter;
     private final InputStream stream;
@@ -22,7 +22,6 @@ public class Stream implements DataSource{
 
     /**
      *
-     * @param f - formatter to push data to
      * @param s - stream to read data from
      * @param delimiter - byte delimiter to delimit each datum
      * @param c - charset the data in the stream is streamed in
@@ -34,24 +33,28 @@ public class Stream implements DataSource{
     }
 
     public void open() {
-        try {
-            Deque<Byte> segmentBuffer = new ArrayDeque<>();
-            int b;
-            while ((b = stream.read()) != -1) {
-                segmentBuffer.add((byte) b);
-                if (b == delimiter) {
-                    int bufLen = segmentBuffer.size();
-                    byte[] segmentBytes = new byte[bufLen];
-                    for (int i = 0; i < bufLen; i++) {
-                        segmentBytes[i] = segmentBuffer.remove();
+        if (this.formatter == null) {
+            System.out.println("No Parser available for datasource");
+        } else {
+            try {
+                Deque<Byte> segmentBuffer = new ArrayDeque<>();
+                int b;
+                while ((b = stream.read()) != -1) {
+                    segmentBuffer.add((byte) b);
+                    if (b == delimiter) {
+                        int bufLen = segmentBuffer.size();
+                        byte[] segmentBytes = new byte[bufLen];
+                        for (int i = 0; i < bufLen; i++) {
+                            segmentBytes[i] = segmentBuffer.remove();
+                        }
+                        String datum = new String(segmentBytes, charset);
+                        formatter.parseData(datum);
                     }
-                    String datum = new String(segmentBytes, charset);
-                    formatter.parseData(datum);
                 }
+            } catch (IOException e) {
+                System.err.println("Cannot read from stream!");
+                System.err.println(e.getMessage());
             }
-        } catch (IOException e) {
-            System.err.println("Cannot read from stream!");
-            System.err.println(e.getMessage());
         }
     }
 
@@ -59,7 +62,6 @@ public class Stream implements DataSource{
         stream.close();
     }
 
-    @Override
     public void linkParser(DataFormat f) {
         this.formatter = f;
     }
