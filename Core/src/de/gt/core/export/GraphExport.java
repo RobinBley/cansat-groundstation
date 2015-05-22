6a1a24805dd727ec6aa271caf2a977cd40b1ad1d
Robin Bley <robin-bley@hotmail.de>
2015-05-22 12:58:12 +0200

    export import gefixed

--- Core/src/de/gt/core/export/GraphExport.java
+++ Core/src/de/gt/core/export/GraphExport.java
@@ -8,8 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import org.openide.util.lookup.ServiceProvider;
 
