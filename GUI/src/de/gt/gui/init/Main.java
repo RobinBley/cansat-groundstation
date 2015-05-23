9c96e410d57379cfb527994dda32568dbc19d65d
Kevin Neumeyer <kevin@kpro-media.de>
2015-05-23 13:00:06 +0200

    Preset für das Testen vom Config-Loader

--- GUI/src/de/gt/gui/init/Main.java
+++ GUI/src/de/gt/gui/init/Main.java
@@ -1,6 +1,11 @@
 package de.gt.gui.init;
 
+import java.awt.TrayIcon;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 import org.openide.modules.ModuleInstall;
+import org.openide.util.Exceptions;
 
 /**
  * Startup script
@@ -9,6 +14,15 @@ import org.openide.modules.ModuleInstall;
 public class Main extends ModuleInstall {
     @Override
     public void restored(){
+        /*
+            Beim ZIP-Package: Hauptordner der Applikation als CWD
+            Beim Standard-Debug-Build GUI/ als CWD
+        */
+        File configDir = new File("config");
         
+        if(!configDir.exists()){
+            //Ordner für Config erstellen
+            configDir.mkdir();
+        }
     }
 }
