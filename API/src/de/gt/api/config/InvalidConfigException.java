33881383223e689e58dd52ced8ad2227714628f8
Kevin Neumeyer <kevin@kpro-media.de>
2015-05-25 15:35:26 +0200

    Unnötige Formatierungsanpassungen

--- API/src/de/gt/api/config/InvalidConfigException.java
+++ API/src/de/gt/api/config/InvalidConfigException.java
@@ -2,6 +2,8 @@ package de.gt.api.config;
 
 /**
  * Config thrown when a config cannot be parsed
+ *
  * @author Kevin
  */
-public class InvalidConfigException extends Exception{}
+public class InvalidConfigException extends Exception {
+}
