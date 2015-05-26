d34eeb6a52c2c084bf7351af52f6bad44bdeaac9
Kevin Neumeyer <kevin@kpro-media.de>
2015-05-26 14:55:35 +0200

    Experimenteller Reflection Ansatz um Datenquellen mit Konstruktorargumenten per Konfigurationsdialog zu initlaisieren

--- API/src/de/gt/api/sources/DataSource.java
+++ API/src/de/gt/api/sources/DataSource.java
@@ -2,6 +2,7 @@ package de.gt.api.sources;
 
 import de.gt.api.input.dataformat.DataFormat;
 import java.io.Closeable;
+import javax.swing.JDialog;
 
 /**
  * Interface für alle Datenquellen
@@ -9,17 +10,22 @@ import java.io.Closeable;
  * @author mhuisi
  */
 public interface DataSource extends Closeable {
-    
+
     /**
      * Öffnet diese Quelle
      */
     void open();
 
     /**
-     * Linkt einen Parser als Zwischenstück 
-     * für die Weiterverarbeitung in die Pipeline
+     * Linkt einen Parser als Zwischenstück für die Weiterverarbeitung in die
+     * Pipeline
      *
      * @param f - formatter
      */
     void linkParser(DataFormat f);
+
+    default DataSourceConfigurationDialog getConfigurationDialog() {
+        //Null meint hier es ist keine Konfiguration der Datenquelle über einen Dialog erforderlich
+        return null;
+    }
 }
---
+++ API/src/de/gt/api/sources/DataSourceConfiguration.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gt.api.sources;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+/**
+ *
+ * @author Kevin
+ */
+public class DataSourceConfiguration {
+
+    Map<Class, Object> configuration = new ConcurrentSkipListMap<>();
+
+    public void setConfigurationItem(Class key, Object item) {
+        configuration.put(key, item);
+    }
+
+    public Set<Class> getParamSignature() {
+        //Parametersignatur zum Verfgleich mit dem entsprechendem Datenquellenkonstruktor zurückgeben
+        return configuration.keySet();
+    }
+
+    public Object[] getParams() {
+        //Parameter für Klasseninstanziierung zurückgeben
+        return configuration.values().toArray();
+    }
+}
