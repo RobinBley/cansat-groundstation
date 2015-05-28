56614c912e8d76bf566cc1baeb62c7d08db49427
mhuisi <marc.huisinga@freenet.de>
2015-05-28 22:04:51 +0200

    Removed map visualisation annotations

--- GUI/src/de/gt/gui/window/EarthTopComponent.java
+++ GUI/src/de/gt/gui/window/EarthTopComponent.java
@@ -7,24 +7,23 @@ package de.gt.gui.window;
 
 import de.gt.api.config.Config;
 import de.gt.api.gps.GPSKey;
-import de.gt.api.relay.Receiver;
 import gov.nasa.worldwind.BasicModel;
 import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
 import gov.nasa.worldwind.geom.Position;
 import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.render.Annotation;
 import gov.nasa.worldwind.render.AnnotationAttributes;
 import gov.nasa.worldwind.render.GlobeAnnotation;
 import gov.nasa.worldwind.render.Polyline;
+import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.swing.JCheckBox;
 import org.openide.windows.TopComponent;
 import org.openide.util.NbBundle.Messages;
 
-/**
- * DO NOT TOUCH WITH THE DESIGNER!
- */
 @TopComponent.Description(
         preferredID = "LivePathEarthTopComponent",
         //iconBase="SET/PATH/TO/ICON/HERE", 
@@ -38,14 +37,8 @@ import org.openide.util.NbBundle.Messages;
 })
 public final class EarthTopComponent extends DataReceiverComponent {
 
-    private static final AnnotationAttributes ATTRIBS = new AnnotationAttributes();
-
-    static {
-        ATTRIBS.setAdjustWidthToText(AVKey.SIZE_FIT_TEXT);
-    }
-
     private final List<Position> positions = new ArrayList<>();
-
+    
     private final GPSKey k;
 
     private WorldWindowGLCanvas wwd;
@@ -61,29 +54,20 @@ public final class EarthTopComponent extends DataReceiverComponent {
         setToolTipText(Bundle.HINT_LivePathEarthTopComponent());
     }
 
-    private void add(Position p, GlobeAnnotation a) {
-        positions.add(p);
-        path.setPositions(positions);
-        layer.addRenderable(a);
-    }
-
     @Override
     public void receive(Map<String, Double> datum) {
         double latitude = datum.get(k.getLatitudeKey());
         double longitude = datum.get(k.getLongitudeKey());
         double altitude = datum.get(k.getAltitudeKey());
         Position p = Position.fromDegrees(latitude, longitude, altitude);
-        StringBuilder b = new StringBuilder();
-        datum.entrySet().stream()
-                .map(e -> String.format("%s: %s\n", e.getKey(), e.getValue()))
-                .forEach(b::append);
-        GlobeAnnotation a = new GlobeAnnotation(b.toString(), p, ATTRIBS);
-        add(p, a);
+        positions.add(p);
+        path.setPositions(positions);
     }
 
     /**
-     * DO NOT TOUCH DO NOT OPEN WITH EDITOR! NBP doesn't allow to add complex
-     * custom components using the editor, manual code editing was needed.
+     * Diesen Code NICHT mit dem Designer berühren!
+     * Der Designer erlaubt es nicht, komplexe Komponenten wie 
+     * GL Canvasse einfach zu nutzen.
      */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">
     private void initComponents() {
@@ -94,19 +78,11 @@ public final class EarthTopComponent extends DataReceiverComponent {
         path = new Polyline();
         layer.addRenderable(path);
         bm.getLayers().add(layer);
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGap(0, 400, Short.MAX_VALUE)
-                .addComponent(wwd, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGap(0, 300, Short.MAX_VALUE)
-                .addComponent(wwd, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
-        );
+        setLayout(new BorderLayout(0, 0));
+        add(wwd, BorderLayout.CENTER);
     }// </editor-fold
+    // Diese fehlende Klammer sorgt dafür, dass der manuelle Code
+    // nicht von NBP überschrieben werden kann
 
     @Override
     public void configChanged(Config newConfig) {
@@ -117,7 +93,6 @@ public final class EarthTopComponent extends DataReceiverComponent {
     public void clearData() {
         //Traces entfernen
         layer.removeAllRenderables();
-
         //Polyline wieder aufbauen
         path = new Polyline();
         layer.addRenderable(path);
